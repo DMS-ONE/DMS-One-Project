cf47c0cf348bb13dd78a723409df522874aa361c
leesin <dragon02229@naver.com>
2023-01-10 22:57:31 +0900

    ⚡ :: Made BaseActivity
    it's make easy to activity

---
+++ presentation/src/main/java/com/yongjincompany/app/util/BaseActivity.kt
@@ -0,0 +1,34 @@
+package com.yongjincompany.app.util
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    private lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+        initView()
+    }
+
+    fun showShortToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showLongToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    open fun initView() {}
+}

f8b1482a3ee067ba7f429273c6e4f7dcea0adf58
leesin <dragon02229@naver.com>
2023-01-10 22:50:34 +0900

    ⚡ :: Implement GitHubProfileApi
    add GitHubProfile Response too

---
+++ data/src/main/java/com/yongjincompany/data/remote/response/profile/GitHubProfileResponse.kt
@@ -0,0 +1,12 @@
+package com.yongjincompany.data.remote.response.profile
+
+import com.google.gson.annotations.SerializedName
+
+data class GitHubProfileResponse(
+    @SerializedName("avatar_url") val avatarUrl: String,
+    @SerializedName("bio") val bio: String,
+    @SerializedName("followers") val followers: Int,
+    @SerializedName("following") val following: Int,
+    @SerializedName("login") val login: String,
+    @SerializedName("name") val name: String,
+)

19d326e360c1bfdfff1b735c1562bb89866e261b
leesin <dragon02229@naver.com>
2023-01-11 09:22:21 +0900

    🐛 :: fix don't reading binding in Activity
    private -> protected

--- presentation/src/main/java/com/yongjincompany/app/util/BaseActivity.kt
+++ presentation/src/main/java/com/yongjincompany/app/util/BaseActivity.kt
@@ -11,7 +11,7 @@ abstract class BaseActivity<B : ViewDataBinding>(
     @LayoutRes private val layoutId: Int
 ) : AppCompatActivity() {
 
-    private lateinit var binding: B
+    protected lateinit var binding: B
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

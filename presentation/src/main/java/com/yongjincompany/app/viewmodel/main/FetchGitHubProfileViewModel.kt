db5858248414ec426dfc5ca32facddb8f3b0247b
leesin <dragon02229@naver.com>
2023-01-11 11:35:51 +0900

    ⚡ :: Implment FetchGitHubProfileViewModel

--- presentation/src/main/java/com/yongjincompany/app/viewmodel/main/FetchGitHubProfileViewModel.kt
+++ presentation/src/main/java/com/yongjincompany/app/viewmodel/main/FetchGitHubProfileViewModel.kt
@@ -4,14 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yongjincompany.app.util.MutableEventFlow
 import com.yongjincompany.app.util.asEventFlow
+import com.yongjincompany.app.viewmodel.main.FetchGitHubProfileViewModel.FetchGitHubProfileEvent.*
 import com.yongjincompany.domain.entity.profile.GitHubProfileEntity
-import com.yongjincompany.domain.exception.BadRequestException
 import com.yongjincompany.domain.exception.ForbiddenException
 import com.yongjincompany.domain.exception.UnauthorizedException
+import com.yongjincompany.domain.exception.UnknownException
 import com.yongjincompany.domain.usecase.profile.GitHubProfileUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class FetchGitHubProfileViewModel @Inject constructor(
     private val gitHubProfileUseCase: GitHubProfileUseCase
 ) : ViewModel() {
@@ -23,18 +26,29 @@ class FetchGitHubProfileViewModel @Inject constructor(
         viewModelScope.launch {
             kotlin.runCatching {
                 gitHubProfileUseCase.execute(userName).collect {
-                    FetchGitHubProfileEvent.FetchGitHubProfile(it)
+                    event(FetchGitHubProfile(it.toData()))
                 }
             }.onFailure {
                 when (it) {
                     is ForbiddenException -> {
-                        FetchGitHubProfileEvent.ErrorMessage("권한 때문에 요청이 거절되었습니다")
+                        event(ErrorMessage("권한 때문에 요청이 거절되었습니다"))
                     }
+
                     is UnauthorizedException -> {
-                        FetchGitHubProfileEvent.ErrorMessage("인증이 필요합니다")
+                        event(ErrorMessage("인증이 필요합니다"))
+                    }
+
+                    else -> {
+                        event(ErrorMessage("알 수 없는 에러"))
                     }
                 }
             }
+        }
+    }
+
+    private fun event(event: FetchGitHubProfileEvent) {
+        viewModelScope.launch {
+            _fetchGitHubProfileEvent.emit(event)
         }
     }
 
@@ -44,4 +58,14 @@ class FetchGitHubProfileViewModel @Inject constructor(
 
         data class ErrorMessage(val errorMessage: String) : FetchGitHubProfileEvent()
     }
+
+    private fun GitHubProfileEntity.toData() =
+        GitHubProfileEntity(
+            avatarUrl = avatarUrl,
+            bio = bio,
+            followers = followers,
+            following = following,
+            login = login,
+            name = name,
+        )
 }

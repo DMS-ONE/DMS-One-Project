1d8443b12d2b039536485466ef7510c9d93d5268
Tmdhoon2 <hks026naver@gmail.com>
2023-01-14 23:41:11 +0900

    ⚡ :: Implement intent logic

--- presentation/src/main/java/com/yongjincompany/app/feature/home/SelectActivity.kt
+++ presentation/src/main/java/com/yongjincompany/app/feature/home/SelectActivity.kt
@@ -1,10 +1,12 @@
 package com.yongjincompany.app.feature.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.yongjincompany.app.databinding.ActivitySelectBinding
 import com.yongjincompany.app.util.BaseActivity
 import com.yongjincompany.app.R
+import com.yongjincompany.app.feature.dledledle.SeunghoonActivity
 import com.yongjincompany.app.util.loadFromUrl
 import com.yongjincompany.app.util.repeatOnStarted
 import com.yongjincompany.app.viewmodel.main.FetchGitHubProfileViewModel
@@ -25,6 +27,10 @@ class SelectActivity : BaseActivity<ActivitySelectBinding>(
 
         binding.cvJungseunghoonCard.setOnClickListener {
             //TODO : 승훈 카드 눌렀을 때 넘어가는 동작 만들어주기
+            startActivity(Intent(
+                this,
+                SeunghoonActivity::class.java,
+            ))
         }
 
         binding.cvYongjinCard.setOnClickListener {

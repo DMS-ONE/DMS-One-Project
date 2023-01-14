328d2b6d8f48bfa0f091436a49aa472143ed8132
Tmdhoon2 <hks026naver@gmail.com>
2023-01-14 23:39:20 +0900

    ⚡ :: add SeunghoonActivity

---
+++ presentation/src/main/java/com/yongjincompany/app/feature/dledledle/SeunghoonActivity.kt
@@ -0,0 +1,13 @@
+package com.yongjincompany.app.feature.dledledle
+
+import com.yongjincompany.app.R
+import com.yongjincompany.app.databinding.ActivitySelectBinding
+import com.yongjincompany.app.util.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SeunghoonActivity : BaseActivity<ActivitySelectBinding>(
+    R.layout.activity_seunghoon,
+) {
+
+}

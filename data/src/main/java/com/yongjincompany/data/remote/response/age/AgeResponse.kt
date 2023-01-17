355ea6f2db3e444f55d4089665af6125c18f96a2
Tmdhoon2 <hks026naver@gmail.com>
2023-01-17 19:54:31 +0900

    ⚡ :: implement AgeEntity

--- data/src/main/java/com/yongjincompany/data/remote/response/age/AgeResponse.kt
+++ data/src/main/java/com/yongjincompany/data/remote/response/age/AgeResponse.kt
@@ -3,7 +3,7 @@ package com.yongjincompany.data.remote.response.age
 import com.google.gson.annotations.SerializedName
 
 data class AgeResponse(
-    @SerializedName("age") val age: Int,
+    @SerializedName("age") val age: Int?,
     @SerializedName("count") val count: Int,
     @SerializedName("country_id") val countryId: Int,
     @SerializedName("name") val name: String,
---
+++ domain/src/main/java/com/yongjincompany/domain/entity/age/AgeEntity.kt
@@ -0,0 +1,8 @@
+package com.yongjincompany.domain.entity.age
+
+data class AgeEntity(
+    val age: Int?,
+    val count: Int,
+    val countryId: Int,
+    val name: String,
+)

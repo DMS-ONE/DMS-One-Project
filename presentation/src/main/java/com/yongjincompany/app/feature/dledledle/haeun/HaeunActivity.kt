d21fb5c0912df0390fb1c57cbabbeb769648ec30
haeun <haeun3882@gmail.com>
2023-01-18 00:00:12 +0900

    ⚡:: add HaeunActivity

---
+++ presentation/src/main/java/com/yongjincompany/app/feature/dledledle/haeun/HaeunActivity.kt
@@ -0,0 +1,11 @@
+package com.yongjincompany.app.feature.dledledle.haeun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HaeunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_haeun)
+    }
+}

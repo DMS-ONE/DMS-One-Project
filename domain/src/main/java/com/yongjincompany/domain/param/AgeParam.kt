e04b526d3b15484b4ab8b1719c65f75f272c18c9
Tmdhoon2 <hks026naver@gmail.com>
2023-01-17 20:39:57 +0900

    ⚡ :: implement AgeUseCase

---
+++ domain/src/main/java/com/yongjincompany/domain/param/AgeParam.kt
@@ -0,0 +1,6 @@
+package com.yongjincompany.domain.param
+
+data class AgeParam(
+    val name: String,
+    val countryId: String,
+)
---
+++ domain/src/main/java/com/yongjincompany/domain/usecase/age/AgeUseCase.kt
@@ -0,0 +1,19 @@
+package com.yongjincompany.domain.usecase.age
+
+import com.yongjincompany.domain.entity.age.AgeEntity
+import com.yongjincompany.domain.param.AgeParam
+import com.yongjincompany.domain.repository.AgeRepository
+import com.yongjincompany.domain.usecase.UseCase
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class AgeUseCase @Inject constructor(
+    private val ageRepository: AgeRepository,
+) : UseCase<AgeParam, Flow<AgeEntity>>() {
+    override suspend fun execute(data: AgeParam): Flow<AgeEntity> {
+        ageRepository.fetchAge(
+            name = data.name,
+            countryId = data.countryId,
+        )
+    }
+}
